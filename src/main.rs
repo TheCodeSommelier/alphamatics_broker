b019fabca78fbc894c5b9466893ddfc39bdf7076
Tony Masek <tm@tony-masek.com>
2026-02-19 17:55:07 +0100

    feat: add sentry

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::io;
+use std::{env, io};
 use tokio::net::{TcpListener, TcpStream};
 
 use crate::units::teltonika::{teltonika_listen, utils::teltonika_read_imei};
@@ -20,14 +20,46 @@ async fn process_socket(mut socket: TcpStream) -> io::Result<()> {
     Ok(())
 }
 
-#[tokio::main]
-async fn main() -> io::Result<()> {
+async fn tokio_main() -> io::Result<()> {
     let addr = "127.0.0.1:4001";
     let listener = TcpListener::bind(addr).await?;
 
     loop {
         println!("Listening on: {addr}");
-        let (socket, _) = listener.accept().await?;
-        process_socket(socket).await?;
+
+        match listener.accept().await {
+            Ok((socket, _)) => {
+                if let Err(err) = process_socket(socket).await {
+                    sentry::capture_error(&err);
+                    eprintln!("socket error: {err}");
+                }
+            }
+            Err(err) => {
+                sentry::capture_error(&err);
+                eprintln!("accept error: {err}");
+            }
+        }
     }
+}
+fn main() {
+    let _guard = sentry::init((
+        env::var("SETRY_DNS").expect("SETRY_DNS must be set"),
+        sentry::ClientOptions {
+            release: sentry::release_name!(),
+            send_default_pii: true,
+            traces_sample_rate: 0.1,
+            ..Default::default()
+        },
+    ));
+
+    tokio::runtime::Builder::new_multi_thread()
+        .enable_all()
+        .build()
+        .unwrap()
+        .block_on(async {
+            let result = tokio_main().await;
+            if let Err(err) = &result {
+                sentry::capture_error(err);
+            }
+        });
 }
